8174bd12b42e7a68c0b632ef3fe6352873b28436
hrnarimani <hrnarimani65@gmail.com>
2023-06-07 11:40:01 +0330

    commite num 2 - correct index contact and layout html code

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -6,11 +6,10 @@ namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -18,12 +17,12 @@ namespace DevCard.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Contact()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
