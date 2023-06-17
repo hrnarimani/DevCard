b8a5c5ff4b98464f6dd9b0115be5a17fc3d83be0
hrnarimani <hrnarimani65@gmail.com>
2023-06-17 10:59:39 +0430

    commite for view components

---
+++ DevCard/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard.ViewComponents
+{
+	public class LatestArticlesViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke(string name)
+
+		{
+			var articles = new List<Article>
+			{
+				new Article(1, "تاکسی", "درخواست آفلاین تاکسی برای سفر های درون شهری", "blog-post-thumb-1.jpg"),
+				new Article(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور", "blog-post-thumb-2.jpg"),
+				new Article(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "blog-post-thumb-3.jpg"),
+				new Article(4, "فضاپیما", "برنامه مدیریت فضاپیما های ناسا", "blog-post-thumb-4.jpg"),
+			};
+			return View("_Latestarticles",articles);	
+		}
+		 
+	}
+}
