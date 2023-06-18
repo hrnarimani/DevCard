ccb1097c07d2c0a1226f75f3c2f5dc34825d6687
hrnarimani <hrnarimani65@gmail.com>
2023-06-18 10:46:33 +0430

    data anatoation client and server

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using DevCard.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net;
+using DevCard_MVC.Models;
 
 namespace DevCard.Controllers
 {
@@ -17,12 +19,39 @@ namespace DevCard.Controllers
             return View();
         }
 
-		public IActionResult Contact()
-		{
-			return View();
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            var model = new Contact();
+			return View(model);
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+           // if (ModelState.IsValid == false)
+           if (!ModelState.IsValid)
+           {
+               ViewBag.error = "اطلاعات وارد شده صحیح نمی باشد";
+               return View(model);
+           }
+
+           ViewBag.success = "پیغام شما با موفقیت ثبت گردید";
+           return View();
+           //return RedirectToAction("Index");
+        }
+
+        //for Downloadfile
+        //public FileResult Index()
+        //{
+        //    //return File("~/test.txt", "text/html");
+        //    var fileByte = System.IO.File.ReadAllBytes("wwwroot/test.txt");
+        //    const string fileName = "testFile.txt";
+        //    return File(fileByte, MediaTypeNames.Text.Plain, fileName);
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
